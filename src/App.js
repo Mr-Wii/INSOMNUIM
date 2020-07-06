ee09024cbb8b5640c34b86b5d6cd09069f2122e7
Mr-Wii <erubakugeki@gmail.com>
2020-07-06 19:51:07 +0100

    intro one

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import './App.css'
+import './index.css'
+import Intro from './components/intro'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>INSOMNIUM</p>
-      </header>
-    </div>
-  )
+const App = () => {
+  return <Intro />
 }
 
 export default App
---
+++ src/components/intro.jsx
@@ -0,0 +1,48 @@
+import React, {
+  useEffect,
+  useContext,
+  useCallback,
+  useState,
+  useRef,
+} from 'react'
+import styled from 'styled-components'
+import TextyAnim from 'rc-texty'
+
+const Intro = () => {
+  const [show, setShow] = useState('I N S O M N I U M')
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShow(!show)
+    }, 4000)
+    return () => clearInterval(interval)
+  }, [])
+  return (
+    <MainDiv>
+      <Texty
+        type="alpha"
+        duration="2000"
+        mode="random"
+        onEnd={(type) => {
+          console.log(type) // tslint:disable-line
+        }}
+      >
+        {show}
+      </Texty>
+    </MainDiv>
+  )
+}
+
+export default Intro
+
+const MainDiv = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const Texty = styled(TextyAnim)`
+  color: white;
+  font-size: 7rem;
+  font-family: 'Piedra', cursive;
+  cursor: pointer;
+`
